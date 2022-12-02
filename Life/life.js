06857ce68d84d027075372ab9368f0d290af6a83
SrLuc <ruanlucenaramos@gmail.com>
2022-12-02 01:58:42 -0300

    att

---
+++ Life/life.js
@@ -0,0 +1,6 @@
+"use strict";
+const influenzaA_H1N2 = { homeostasis: false, membrane: false, organelles: false, geneticMaterial: "DNA", };
+const influenzaA_H2N8 = { homeostasis: false, membrane: false, organelles: false, geneticMaterial: "RNA", };
+console.log(influenzaA_H1N2);
+const Salmonella = { homeostasis: false, membrane: true, organelles: true, geneticMaterial: "RNA", };
+const ClostridiumTetani = { homeostasis: true, membrane: true, organelles: true, geneticMaterial: "DNA", };
---
+++ Life/life.ts
@@ -0,0 +1,17 @@
+interface Life {
+	homeostasis: boolean;
+	membrane: boolean;
+	organelles: boolean;
+	geneticMaterial: string;
+}
+
+interface Virus extends Life {}
+const influenzaA_H1N2: Virus = {homeostasis: false, membrane: false, organelles: false, geneticMaterial: "DNA",};
+const influenzaA_H2N8: Virus = {homeostasis: false, membrane: false, organelles: false, geneticMaterial: "RNA",};
+console.log(influenzaA_H1N2KT);
+
+interface Bacterium extends Life {}
+const Salmonella: Bacterium = {homeostasis: false, membrane: true, organelles: true, geneticMaterial: "RNA",};
+const  ClostridiumTetani: Bacterium = {homeostasis: true, membrane: true, organelles: true, geneticMaterial: "DNA",};
+
+
